34454b3226da8a3ecb3d055abc6d6ded7603ffed
xdJidx <bernardjerome59@gmail.com>
2023-05-30 13:54:56 +0200

    Print alphabet with range

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-for ascii_code in range(ord('a'), ord('z') + 1):
-    print(chr(ascii_code), end='')
-
-print()
+for letter in range(ord('a'), ord('z')+1):
+    print('{}'.format(chr(letter)), end='')
