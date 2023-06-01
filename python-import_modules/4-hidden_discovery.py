6c6b74dd55254ab2052390b635206896e4d82146
xdJidx <bernardjerome59@gmail.com>
2023-06-01 09:28:42 +0200

    Fixe code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for index in dir(hidden_4):
-        if not index.startswitch('__'):
-            print("{:s}".format(index))
+    name = dir(hidden_4)
+    for names in name:
+        if names [:2] != "__":
+            print(names)
