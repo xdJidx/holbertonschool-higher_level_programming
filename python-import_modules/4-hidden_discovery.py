3aaa302c2f5d87cce3352f7cc3c7ff05c77aadfd
xdJidx <bernardjerome59@gmail.com>
2023-06-01 09:30:10 +0200

    Fixe code pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     name = dir(hidden_4)
     for names in name:
-        if names [:2] != "__":
+        if names[:2] != "__":
             print(names)
