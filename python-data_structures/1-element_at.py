c8edbee1b80137a7be9523a0fbf09c75d2ec8c16
xdJidx <bernardjerome59@gmail.com>
2023-06-05 10:41:50 +0200

    Update code with >=

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     lengList = len(my_list)
-    if idx > lengList:
+    if idx >= lengList:
         return None
     return my_list[idx]
