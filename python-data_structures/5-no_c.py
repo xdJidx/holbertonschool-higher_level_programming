bd959bafd8375b562f304c93068f74f574fe3418
xdJidx <bernardjerome59@gmail.com>
2023-06-05 14:05:14 +0200

    Fixe new code with join

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = ''
-
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
+    new_string = ''.join(char for char in my_string if char not in "cC")
     return new_string
