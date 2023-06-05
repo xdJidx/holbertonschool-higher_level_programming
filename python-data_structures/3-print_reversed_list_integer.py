b7d9040f7287a1a62e5421d6cd27150d64c35262
xdJidx <bernardjerome59@gmail.com>
2023-06-05 11:23:30 +0200

    Fixe code with enumerate

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    index = len(my_list) - 1
-    while index >= 0:
-        print("{}".format(my_list[index]))
-        index -= 1
+    if my_list is None:
+        return None
+    else:
+        for index, value in enumerate(reversed(my_list)):
+            print("{:d}".format(value))
