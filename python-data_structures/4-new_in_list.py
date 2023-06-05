2f867ad2e5de29c70af50f20b536db9df3f030c5
xdJidx <bernardjerome59@gmail.com>
2023-06-05 13:45:46 +0200

    Fixe code remove None

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is None:
-        return None
-    else :
-        if idx < 0 or idx >= len(my_list):
-            return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
     new_list = my_list[:]
     new_list[idx] = element
     return new_list
