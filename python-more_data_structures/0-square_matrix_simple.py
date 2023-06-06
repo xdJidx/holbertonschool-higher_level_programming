91939655b37b0f4f499c6e58e05527d6bf4892af
xdJidx <bernardjerome59@gmail.com>
2023-06-06 10:07:19 +0200

    Fixe pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    new_matrix = [[0 for _ in range(len(matrix[0]))]
+                  for _ in range(len(matrix))]
 
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
