d515fc9d6008d668bcb3613676b56a89be45ebe9
xdJidx <bernardjerome59@gmail.com>
2023-06-06 15:39:37 +0200

    fix pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
 
     rom_value = {"I": 1, "V": 5, "X": 10, "L": 50,
-                         "C": 100, "D": 500,"M": 1000}
+                         "C": 100, "D": 500, "M": 1000}
 
     total = 0
     prev_value = 0
