638815e3148a5dcd50065a8969de5db28c0ce4e4
xdJidx <bernardjerome59@gmail.com>
2023-06-06 15:37:42 +0200

    Fix pycodestyle and error code

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or roman_string is None:
-        return None
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
 
     rom_value = {"I": 1, "V": 5, "X": 10, "L": 50,
                          "C": 100, "D": 500,"M": 1000}
@@ -18,6 +18,3 @@ def roman_to_int(roman_string):
         prev_value = value
 
     return total
-
-
-
