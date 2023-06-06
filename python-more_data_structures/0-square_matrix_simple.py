828b3afa2968be4b226e94b5bef647795e107222
xdJidx <bernardjerome59@gmail.com>
2023-06-06 10:25:02 +0200

    Add Not condition

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+
+    if not matrix:
+        return None
+
     new_matrix = [[0 for _ in range(len(matrix[0]))]
                   for _ in range(len(matrix))]
 
