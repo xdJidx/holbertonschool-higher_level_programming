9283915e2e8111c2e3645d4f934cde70c37dbb74
xdJidx <bernardjerome59@gmail.com>
2023-06-12 15:28:44 +0200

    Print 2 square

---
+++ python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 4-square.py)"""
+
+
+class Square:
+    """defined square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = 0
+        self.size = size
+        self.__position = 0
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(coord, int) and coord >= 0 for coord in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(' ' * self.__position[0] + "#" * self.__size)
