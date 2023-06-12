5ff96d2bf81df34ac52adea925dcc2e098376476
xdJidx <bernardjerome59@gmail.com>
2023-06-12 14:27:41 +0200

    Define instance size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """class that defines a square"""
+    def __init__(self, size): # initial value size for square
+        self.__size = size # private instance attribute for square size
