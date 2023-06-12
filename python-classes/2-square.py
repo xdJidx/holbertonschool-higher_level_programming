b38e225cd592d617310f39db4b1e0cc33fc4705d
xdJidx <bernardjerome59@gmail.com>
2023-06-12 14:36:58 +0200

    Task 2 add condition for size

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """defined square by: (based on 1-square.py)"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
