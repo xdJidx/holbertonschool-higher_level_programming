48c41a185316baeb2be3c01c676de66d48b51795
xdJidx <bernardjerome59@gmail.com>
2023-06-12 14:29:34 +0200

    pycodestyle 2 space before command

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,5 +6,5 @@ Write a class Square that defines a square by: (based on 0-square.py)
 
 class Square:
     """class that defines a square"""
-    def __init__(self, size): # initial value size for square
-        self.__size = size # private instance attribute for square size
+    def __init__(self, size):  # initial value size for square
+        self.__size = size  # private instance attribute for square size
