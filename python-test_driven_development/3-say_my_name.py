2ba7c8f896c8376d965723d69fe745f5dc0ee14f
xdJidx <bernardjerome59@gmail.com>
2023-06-17 15:04:01 +0200

    Correction pycodestyle and add more test

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,8 @@
 
 def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
+        raise TypeError(
+            "first_name must be a string or last_name must be a string")
 
     if last_name:
         print(f"My name is {first_name} {last_name}")
