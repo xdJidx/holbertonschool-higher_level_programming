b91d051644774701ee10d3567780a80fb3958328
xdJidx <bernardjerome59@gmail.com>
2023-06-19 16:23:44 +0200

    Add Exeption for class BaseGeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Based on 5-base_geometry.py
+Defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """Define an area"""
+
+    def area(self):
+        """Only raise an Exception"""
+
+        raise Exception("area() is not implemented")
