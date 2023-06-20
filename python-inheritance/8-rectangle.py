0618e0b1bac7cd8f86d3837ca1025ee1c5bbaad1
xdJidx <bernardjerome59@gmail.com>
2023-06-20 10:35:29 +0200

    Use import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,29 +4,7 @@ Based on 7-base_geometry.py
 Defines an Rectangle inherits from BaseGeometry.
 """
 
-
-class BaseGeometry:
-    """Define an area"""
-
-    def area(self):
-        """Only raise an Exception"""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Resume:
-            Verify if value is a interger
-
-        Args:
-            -name: always string, the name of Geometry.
-            -value: interger to ckeck
-        """
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
