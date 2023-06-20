2b18a16396b592a0346baab2e0c6828980f2b7c7
xdJidx <bernardjerome59@gmail.com>
2023-06-20 10:45:10 +0200

    Print area of rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Based on 8-base_geometry.py
+Defines an Rectangle inherits from BaseGeometry.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Definie les caracteristique le classe Rectangle
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialise la longeur de with et height et verifie si
+        elles sont des integers
+
+        Args:
+            with: longeurs of Rectangle
+            height: largeur of Rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        return (self.__width * self.__height)
