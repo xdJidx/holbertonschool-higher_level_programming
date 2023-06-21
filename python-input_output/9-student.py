305d2c1989821531cdc3076d69c5e32e14fbb2c5
xdJidx <bernardjerome59@gmail.com>
2023-06-21 15:44:01 +0200

    Write a class Student that defines a student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student by"""
+
+
+class Student:
+    """defined students"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
