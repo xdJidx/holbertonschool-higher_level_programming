81590b656f029215062f3438fda1d0d6dc030d13
xdJidx <bernardjerome59@gmail.com>
2023-06-23 09:33:55 +0200

    Correction file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,11 +6,10 @@ Defines an empty class BaseGeometry.
 
 
 class BaseGeometry:
-    """Define an area"""
+    """Reprsent base geometry."""
 
     def area(self):
         """Only raise an Exception"""
-
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -21,9 +20,12 @@ class BaseGeometry:
         Args:
             -name: always string, the name of Geometry.
             -value: interger to ckeck
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
 
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
