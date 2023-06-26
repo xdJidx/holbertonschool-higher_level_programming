99b8989d1cca616cecd6101b7a9c50ac7613a8b1
xdJidx <bernardjerome59@gmail.com>
2023-06-26 15:57:55 +0200

    Update the class Rectangle update(self, *args, **kwargs)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,16 +100,28 @@ class Rectangle(Base):
         string += str(self.__width) + "/" + str(self.__height)
         return string
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Assigns an argument to each attribute
         """
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args is not []:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+
+        if 'id' in kwargs:
+            self.id = kwargs['id']
+        if 'width' in kwargs:
+            self.width = kwargs['width']
+        if 'height' in kwargs:
+            self.height = kwargs['height']
+        if 'x' in kwargs:
+            self.x = kwargs['x']
+        if 'y' in kwargs:
+            self.y = kwargs['y']
