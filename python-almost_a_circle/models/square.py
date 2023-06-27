8802459f762c70384a18c606211af54f4c4eeb6c
xdJidx <bernardjerome59@gmail.com>
2023-06-27 10:18:12 +0200

    Add ditionnary for Square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -55,3 +55,14 @@ class Square(Rectangle):
             self.x = kwargs['x']
         if 'y' in kwargs:
             self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """"Dictionnary of Square
+
+        """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
