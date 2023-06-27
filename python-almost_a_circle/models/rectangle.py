56946aeaa603b2c86274a5ba37f5022750a0dd88
xdJidx <bernardjerome59@gmail.com>
2023-06-27 10:14:23 +0200

    Add dictionnary for class rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -125,3 +125,12 @@ class Rectangle(Base):
             self.x = kwargs['x']
         if 'y' in kwargs:
             self.y = kwargs['y']
+
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
