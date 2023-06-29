478f791d74e3cfb3f503f94620d476eddf14b24a
xdJidx <bernardjerome59@gmail.com>
2023-06-29 14:42:42 +0200

    Correction commentary

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 # test_base.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines unittests for base.py.
-
-Unittest classes:
-    TestBase_instantiation - line 23
-    TestBase_to_json_string - line 110
-    TestBase_save_to_file - line 156
-    TestBase_from_json_string - line 234
-    TestBase_create - line 288
-    TestBase_load_from_file - line 340
-    TestBase_save_to_file_csv - line 406
-    TestBase_load_from_file_csv - line 484
 """
 import os
 import unittest
