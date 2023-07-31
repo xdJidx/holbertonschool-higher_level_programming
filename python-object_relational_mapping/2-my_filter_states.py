3d49ec51057c0f3bde01d133945ea12cbf14576a
xdJidx <bernardjerome59@gmail.com>
2023-07-31 13:40:05 +0200

    Change code in low text

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,26 +9,9 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print(
-            "Usage: ./2-my_filter_states.py <mysql_username>"
-            + "<mysql_password> <database_name> <state_name>")
-        sys.exit(1)
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host='localhost', port=3306, user=mysql_username,
-        passwd=mysql_password, db=database_name)
-    cursor = db.cursor()
-
-    # Exécuter la requête pour récupérer l'État en fonction du nom fourni
-    cursor.execute(
-        "SELECT * FROM `states` WHERE name=%s ORDER BY id ASC", (state_name,))
-    [print(state) for state in cursor.fetchall()]
-
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
