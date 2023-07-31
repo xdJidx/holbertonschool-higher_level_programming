79330003fbfede53e99bec6d98594ba7da908398
xdJidx <bernardjerome59@gmail.com>
2023-07-31 10:00:56 +0200

    Task 0 : Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import mysql.connector as MySQLdb
+import sys
+
+def list_states(username, password, database):
+    try:
+        # Connect to MySQL server running on localhost at standard port 3306
+        connection = MySQLdb.connect(host='localhost', port=3306,
+                                     user=username, passwd=password,
+                                     db=database)
+
+        # Create a cursor to interact with the database
+        cursor = connection.cursor()
+
+        # Execute the SQL query to retrieve all distinct states sorted by id in ascending order
+        cursor.execute("SELECT DISTINCT * FROM states ORDER BY id ASC;")
+
+        # Use a set to store the names encountered
+        seen_names = set()
+
+        # Fetch all rows from the result set
+        states = cursor.fetchall()
+
+        # Display the results as specified in the example
+        for state in states:
+            state_id, state_name = state
+            # Check if the name is already in the set
+            if state_name not in seen_names:
+                seen_names.add(state_name)
+                print(state)
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
+    # Get the arguments from the command line
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Call the function to list states
+    list_states(mysql_username, mysql_password, database_name)
