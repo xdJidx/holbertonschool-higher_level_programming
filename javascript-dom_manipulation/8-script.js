dbb61008480c9b489da2039b9e199e5aaf670883
xdJidx <bernardjerome59@gmail.com>
2023-09-12 09:55:36 +0200

    Correction semistandard

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,21 +1,21 @@
-function fetchHello() {
-    // Select the HTML element with the id "hello"
-    const helloElement = document.getElementById('hello');
+function fetchHello () {
+  // Select the HTML element with the id "hello"
+  const helloElement = document.getElementById('hello');
 
-    const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+  const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
 
-    // Use the Fetch API to retrieve the translation
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        // Extract and display the translation
-        const helloTranslation = data.hello;
-        helloElement.textContent = helloTranslation;
-      })
-      .catch((error) => {
-        console.error('Fetch error:', error);
-      });
-  }
+  // Use the Fetch API to retrieve the translation
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Extract and display the translation
+      const helloTranslation = data.hello;
+      helloElement.textContent = helloTranslation;
+    })
+    .catch((error) => {
+      console.error('Fetch error:', error);
+    });
+}
 
-  // Call the function when the document is ready
-  document.addEventListener('DOMContentLoaded', fetchHello);
+// Call the function when the document is ready
+document.addEventListener('DOMContentLoaded', fetchHello);
