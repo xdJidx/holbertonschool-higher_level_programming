3d6cdaaa4eaea34dc6681d7a2e976d66147cef0b
xdJidx <bernardjerome59@gmail.com>
2023-09-12 09:37:56 +0200

    Change code to display 'Salut' in div hello

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,28 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Le code à l'intérieur de cette fonction sera exécuté après que le DOM soit prêt
-  // Get a reference to the HTML <div> element with id "hello"
-  const helloElement = document.querySelector('header');
+function fetchHello() {
+    // Select the HTML element with the id "hello"
+    const helloElement = document.getElementById('hello');
 
-  // URL of the API endpoint
-  const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+    const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
 
-  // Use the Fetch API to make a GET request to the API endpoint
-  fetch(apiUrl)
-    .then((response) => {
-      // Check if the response status is OK (status code 200)
-      if (response.ok) {
-        // Parse the JSON response
-        return response.json();
-      } else {
-        throw new Error('Failed to fetch translation data');
-      }
-    })
-    .then((data) => {
-      // Display the translation in the "hello" element
-      helloElement.textContent = data.hello;
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      helloElement.textContent = 'Error: Failed to fetch translation data';
-    });
-});
+    // Use the Fetch API to retrieve the translation
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract and display the translation
+        const helloTranslation = data.hello;
+        helloElement.textContent = helloTranslation;
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+  }
+
+  // Call the function when the document is ready
+  document.addEventListener('DOMContentLoaded', fetchHello);
